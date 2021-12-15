f676bbefd0f7875e5adcefb1296e74d5b6fbb5b4
kyle cheeseman <bumbill00@gmail.com>
2021-12-15 17:53:39 +0000

    OctFunction
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ src/main/java/uk/co/benjiweber/expressions/function/OctConsumer.java
@@ -0,0 +1,5 @@
+package uk.co.benjiweber.expressions.function;
+
+public interface OctConsumer<T, U, V, W, X, Y, Z, A> {
+    void accept(T t, U u, V v, W w, X x, Y y, Z z, A a);
+}
---
+++ src/main/java/uk/co/benjiweber/expressions/function/OctFunction.java
@@ -0,0 +1,5 @@
+package uk.co.benjiweber.expressions.function;
+
+public interface OctFunction<A, B, C, D, E, F, G, H, R> {
+    R apply(A a, B b, C c, D d, E e, F f, G g, H h);
+}
