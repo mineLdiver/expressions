57ae139057b3d67808b6ca7eb92c27ebba860f13
kyle cheeseman <bumbill00@gmail.com>
2021-12-15 15:33:41 +0000

    Cursed Sept functions

---
+++ src/main/java/uk/co/benjiweber/expressions/function/SeptConsumer.java
@@ -0,0 +1,5 @@
+package uk.co.benjiweber.expressions.function;
+
+public interface SeptConsumer<T, U, V, W, X, Y, Z> {
+    void accept(T t, U u, V v, W w, X x, Y y, Z z);
+}
---
+++ src/main/java/uk/co/benjiweber/expressions/function/SeptFunction.java
@@ -0,0 +1,5 @@
+package uk.co.benjiweber.expressions.function;
+
+public interface SeptFunction<A, B, C, D, E, F, G, R> {
+    R apply(A a, B b, C c, D d, E e, F f, G g);
+}
